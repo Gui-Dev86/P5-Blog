708397d59ad73a65bdf80d3ac19acbb0fd3a4ee1
Guillaume Vignères <guillaume.vigneres@gmail.com>
2021-11-09 18:04:23 +0100

    maj composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,11 +6,11 @@
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'reference' => NULL,
-        'name' => 'kindertheo/p5',
+        'name' => 'p5gui/blogdev',
         'dev' => true,
     ),
     'versions' => array(
-        'kindertheo/p5' => array(
+        'p5gui/blogdev' => array(
             'pretty_version' => '0.1.0',
             'version' => '0.1.0.0',
             'type' => 'project',
